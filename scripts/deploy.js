947b83c20496ccf78362d206c135cec917a126bc
The Bull Brew <thebullbrew@thebullbrew.com>
2026-06-18 16:10:43 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,28 @@
+const hre = require("hardhat");
 
+async function main() {
+
+    const BREW_ARB =
+        await hre.ethers.getContractFactory(
+            "BREW_ARB"
+        );
+
+    const contract =
+        await BREW_ARB.deploy(
+            "ipfs://YOUR_CID/",
+            "YOUR_WALLET_ADDRESS"
+        );
+
+    await contract.waitForDeployment();
+
+    console.log(
+        "BREW_ARB deployed to:",
+        await contract.getAddress()
+    );
+}
+
+main()
+.catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
